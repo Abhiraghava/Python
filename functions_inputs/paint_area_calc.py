7c2018f59c7574734af1a0d4ed75f7dc88b6afa1
Abhi Raghava <96431633+Abhiraghava@users.noreply.github.com>
2023-08-31 22:58:26 +0530

    Add files via upload

---
+++ functions_inputs/paint_area_calc.py
@@ -0,0 +1,9 @@
+def paint_calc(height,width,cover):
+    number_of_cans=(height*width)/cover
+    print(f"no of cans are :{number_of_cans}")
+
+
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
