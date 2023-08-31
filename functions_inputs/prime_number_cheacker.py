542d01b6a2b2d0500c740a8709dc225af820f52e
Abhi Raghava <96431633+Abhiraghava@users.noreply.github.com>
2023-08-31 23:16:11 +0530

    Add files via upload

---
+++ functions_inputs/prime_number_cheacker.py
@@ -0,0 +1,9 @@
+def prime_checker(number):
+    if n%2==0:
+        print("it is not a prime number")
+    else:
+        print("It is a prime number")
+
+
+n = int(input("Check this number: "))
+prime_checker(number=n)
